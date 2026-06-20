b98a79f082c721807e4a750daa4ff7660999849d
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2026-06-20 10:06:08 +0900

    feat: 流体を背景に追加

--- components/layouts/background.tsx
+++ components/layouts/background.tsx
@@ -1,14 +1,13 @@
 'use client';
 
 import { Canvas } from '@react-three/fiber';
+import { FluidMesh } from './fluidmesh';
 export default function Background() {
   return (
     <div className="canvas-container">
       <Canvas>
-        <mesh>
-          <sphereGeometry />
-          <meshNormalMaterial />
-        </mesh>
+        <color attach="background" args={['#e0f7fa']} />
+        <FluidMesh />
       </Canvas>
     </div>
   );
