8f9c1a543adca94a4dcbd7469615f2a8e3096ae9
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2026-06-23 11:27:35 +0900

    feat: headerの雛形作成

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.scss';
 import Background from '@/components/layouts/background';
+import { Header } from '@/components/layouts/header';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -30,6 +31,7 @@ export default function RootLayout({
     >
       <body className="flex min-h-full flex-col">
         <Background />
+        <Header />
         {children}
       </body>
     </html>
---
+++ components/layouts/header.tsx
@@ -0,0 +1,23 @@
+import { NAV_ITEMS } from '@/constants/navigations';
+import Link from 'next/link';
+
+export const Header = () => {
+  return (
+    <header>
+      <div className="header-container">
+        <Link href="/" className="logo">
+          Portfolio
+        </Link>
+        <nav className="nav-desktop">
+          <ul className="nav-links">
+            {NAV_ITEMS.map((item) => (
+              <li key={item.name}>
+                <Link href={item.href}>{item.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
