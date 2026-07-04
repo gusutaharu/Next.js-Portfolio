93146ffb9a3a3ace5fbf5cb309de7d0f78632af9
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2026-07-05 00:07:14 +0900

    feat:雛形作成

---
+++ components/sections/skills.tsx
@@ -0,0 +1,24 @@
+export const Skills = () => {
+  return (
+    <section id="skills-section">
+      <h2 className="section-title">Skills</h2>
+      <div>
+        <h3 className="section-subtitle">GitHub data</h3>
+        <div></div>
+        <p>
+          ※自身のGithubアカウントのオープンリポジトリの使用言語率を元に算出。
+        </p>
+      </div>
+      <div>
+        <div>
+          <h3 className="section-subtitle">技術スタック</h3>
+          <div></div>
+        </div>
+        <div>
+          <h3 className="section-subtitle">保有資格</h3>
+          <div></div>
+        </div>
+      </div>
+    </section>
+  );
+};
