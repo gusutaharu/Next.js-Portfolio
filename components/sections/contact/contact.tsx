16eccec3f6a9b95a5bad2c11e9394ad68f812d38
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2026-07-30 15:40:04 +0900

    feat: 問い合わせフォームの雛形作成

---
+++ components/sections/contact/contact.tsx
@@ -0,0 +1,15 @@
+export const Contact = () => {
+  return (
+    <section id="contact-section">
+      <h2 className="section-title">Contact</h2>
+      <div className="contact-area">
+        <form action="#" id="contact-form">
+          <input type="text" name="name" placeholder="name" />
+          <input type="email" name="email" placeholder="email" />
+          <textarea name="content" placeholder="message"></textarea>
+          <button className="submit-btn">送信</button>
+        </form>
+      </div>
+    </section>
+  );
+};
