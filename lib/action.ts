36dd4fafe161fd2db5940745cbcdfe20104ea9a0
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2026-08-01 11:16:41 +0900

    feat: zodを用いた入力値の検証を実装

--- lib/action.ts
+++ lib/action.ts
@@ -1,11 +1,26 @@
 'use server';
+import { z } from 'zod';
+
+const formSchema = z.object({
+  name: z
+    .string()
+    .min(2, { error: '名前は二文字以上入力してください' })
+    .max(50, { error: '名前は五十文字以内に収めてください' }),
+  email: z.email({ error: '正しいメールアドレスをご入力ください' }),
+  content: z
+    .string()
+    .min(5, { error: '内容は５文字以上入力してください' })
+    .max(500, { error: '内容は500文字以内に収めてください' }),
+});
 
 export async function sendEmail(formData: FormData) {
-  const name = formData.get('name') as string;
-  const email = formData.get('email') as string;
-  const content = formData.get('content') as string;
+  const validatedData = formSchema.safeParse({
+    name: formData.get('name'),
+    email: formData.get('email'),
+    content: formData.get('content'),
+  });
   try {
-    console.log(name, email, content);
+    console.log('validatedData', validatedData);
   } catch (error) {
     console.log(error);
   }
