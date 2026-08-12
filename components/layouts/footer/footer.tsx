f86d114c92737a90bd99953d0cca04c430be5ae8
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2026-08-12 11:25:52 +0900

    feat: footerの実装

---
+++ components/layouts/footer/footer.tsx
@@ -0,0 +1,14 @@
+export const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-container">
+        <div className="copyright">
+          <span>
+            &copy; {new Date().getFullYear()} &nbsp;gusutaharu&apos;s portfolio.
+            All rights reserved.
+          </span>
+        </div>
+      </div>
+    </footer>
+  );
+};
