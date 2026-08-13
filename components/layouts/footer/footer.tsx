efce23b11de8012937424e80d5b5a97406443250
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2026-08-13 15:28:27 +0900

    feat: footer内のコンテンツを実装

--- components/layouts/footer/footer.tsx
+++ components/layouts/footer/footer.tsx
@@ -1,7 +1,38 @@
+import { NAV_ITEMS } from '@/constants/navigations';
+import Link from 'next/link';
+import { GithubIcon } from '@/components/ui/githubicon';
+import { QiitaIcon } from '@/components/ui/qiitaicon';
+
 export const Footer = () => {
   return (
     <footer>
       <div className="footer-container">
+        <div className="footer-nav">
+          <div className="footer-links">
+            <Link href="/" className="logo-link">
+              <span className="logo-text">gusutaharu</span>
+            </Link>
+            <div className="footer-icons">
+              <GithubIcon />
+              <QiitaIcon />
+            </div>
+          </div>
+          <nav className="footer-navlist">
+            <ul>
+              {NAV_ITEMS.map((item) => (
+                <li key={item.name}>
+                  <Link
+                    href={item.href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {item.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
         <div className="copyright">
           <span>
             &copy; {new Date().getFullYear()} &nbsp;gusutaharu&apos;s portfolio.
